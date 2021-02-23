ce51a35227746b62ab9bc3064a2deaf60bb258b0
TienaViena <irok-vinograd@mail.ru>
2021-02-23 20:43:35 +0300

    additional class for testing data contact created

---
+++ group_contact.py
@@ -0,0 +1,12 @@
+class Group_contact:
+
+    def __init__(self, firstname, lastname, nickname, company, mobile, email, bday, bmonth, byears):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byears = byears
