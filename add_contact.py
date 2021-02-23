5e223888f5c2b3cb7e6e063ec5b02070a1d90df6
TienaViena <irok-vinograd@mail.ru>
2021-02-23 20:43:05 +0300

    additional class for testing data contact created

--- add_contact.py
+++ add_contact.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
+from group_contact import Group_contact
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -12,8 +13,8 @@ class AddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, firstname="Erast", lastname="Fandorin", nickname="Juffin Hally", company="Adventures", mobile="+79991112233",
-                            email="JuffinHally@yandex.ru", bday="14", bmonth="July", byears="1996")
+        self.create_contact(wd, Group_contact(firstname="Erast", lastname="Fandorin", nickname="Juffin Hally", company="Adventures", mobile="+79991112233",
+                            email="JuffinHally@yandex.ru", bday="14", bmonth="July", byears="1996"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -23,39 +24,39 @@ class AddNewContact(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home").click()
 
-    def create_contact(self, wd, firstname, lastname, nickname, company, mobile, email, bday, bmonth, byears):
+    def create_contact(self, wd, group_contact):
         # init new contact
         wd.find_element_by_link_text("add new").click()
         # fill contact
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("%s" % firstname)
+        wd.find_element_by_name("firstname").send_keys("%s" % group_contact.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("%s" % lastname)
+        wd.find_element_by_name("lastname").send_keys("%s" % group_contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("%s" % nickname)
+        wd.find_element_by_name("nickname").send_keys("%s" % group_contact.nickname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("%s" % company)
+        wd.find_element_by_name("company").send_keys("%s" % group_contact.company)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("%s" % mobile)
+        wd.find_element_by_name("mobile").send_keys("%s" % group_contact.mobile)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("%s" % email)
+        wd.find_element_by_name("email").send_keys("%s" % group_contact.email)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("%s" % bday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text("%s" % group_contact.bday)
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("%s" % bmonth)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("%s" % group_contact.bmonth)
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("%s" % byears)
+        wd.find_element_by_name("byear").send_keys("%s" % group_contact.byears)
         # submit new contact
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
